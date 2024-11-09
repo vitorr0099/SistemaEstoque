9fbe8b0e2a72d829f2702bce76f6d6bf44a12ec7
vitorr0099 <vitor.hugo.ramos.dos.santos@univille.br>
2024-11-09 11:58:16 -0300

    feat: add spring database application properties

--- server/src/main/java/vitorr0099/SistemaEstoque/BackendSpringApplication.java
+++ server/src/main/java/vitorr0099/SistemaEstoque/BackendSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaEstoqueApplication {
+public class BackendSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaEstoqueApplication.class, args);
+		SpringApplication.run(BackendSpringApplication.class, args);
 	}
 
 }
